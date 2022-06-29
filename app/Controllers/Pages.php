b06ed5569824804fd3ff319ee725a4e438909da7
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-06-29 21:44:52 +0700

    getRowArrray

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -7,7 +7,7 @@ class Pages extends BaseController
     public function index()
     {
         echo '<h1>Hello World!</h1>';
-        echo $this->db->query("SELECT * FROM users");
+        echo $this->db->query("SELECT * FROM users")->getRowArray();
     }
 
     public function about()
